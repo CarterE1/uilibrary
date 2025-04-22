ba25660968cafd039cf9f82222166baae65481f9
Carter Ellis <54901754+CarterE1@users.noreply.github.com>
2025-04-22 14:56:51 -0400

    Update UILibrary.ts

--- UILibrary.ts
+++ UILibrary.ts
@@ -15,7 +15,6 @@ namespace UILibrary {
     //% group="Elements"
     //% onClick.shadow="callback"
     //% onClick.draggableParameters
-    //% onClick.handlerStatement = true
     //% weight=100
     export function createButton(sprite: Sprite, onClick: () => void): UIElement {
         console.log("Test")
