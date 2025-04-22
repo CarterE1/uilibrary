1b2cb15c7e0555dea4828db8e5463cf345938487
Carter Ellis <54901754+CarterE1@users.noreply.github.com>
2025-04-22 15:15:14 -0400

    Update UILibrary.ts

--- UILibrary.ts
+++ UILibrary.ts
@@ -21,7 +21,7 @@ namespace UILibrary {
     //% block="when $element is clicked run $onClick"
     //% blockId="uilibrary_buttonclickevent"
     //% element.shadow="variables_get"
-    //% sprite.defl="button"
+    //% element.defl="button"
     //% group="Elements"
     //% weight=99
     export function onButtonClick(element: UIElement, onClick: () => void): void {
