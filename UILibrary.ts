336d5c8738395d64d546a80722c1b13b62c7bb43
Carter Ellis <54901754+CarterE1@users.noreply.github.com>
2025-04-22 15:18:59 -0400

    Update UILibrary.ts

--- UILibrary.ts
+++ UILibrary.ts
@@ -18,7 +18,7 @@ namespace UILibrary {
         return new UIElement(sprite)
     }
 
-    //% block="when $element is clicked run $onClick"
+    //% block="when button is clicked run $onClick"
     //% blockId="uilibrary_buttonclickevent"
     //% element.defl="button"
     //% group="Elements"
