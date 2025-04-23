a11c85971b23110caee2697a49bd15f7dcb1a334
Carter Ellis <54901754+CarterE1@users.noreply.github.com>
2025-04-23 18:44:26 -0400

    Update UILibrary.ts

--- UILibrary.ts
+++ UILibrary.ts
@@ -28,7 +28,7 @@ namespace UILibrary {
 
     //% block="create page using $elements"
     //% blockId="uilibrary_createpage"
-    //% elements.shadow="lists_create_with"
+    //% elements.shadow="list"
     //% elements.defl="screen_image_picker"
     //% group="Pages"
     //% weight=100
